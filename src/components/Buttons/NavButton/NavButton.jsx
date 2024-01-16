c4c4051e27236500e173afcc7b8dcaa3f92bd14d
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-01-16 23:57:47 +0400

    creating AnchorNavButton and NavButton components
    Created AnchorNavButton and NavButton reusable components

--- src/components/Buttons/NavButton/NavButton.jsx
+++ src/components/Buttons/NavButton/NavButton.jsx
@@ -1,9 +1,15 @@
 import styles from "./NavButton.module.scss";
 
-export const NavButton = () => {
+export const NavButton = ({ onClick, variant, type, children }) => {
 
   return (
-    <>
-    </>
+    <button
+      onClick={onClick}
+      type={type}
+      className={variant ? `${styles.navButton} ${variant}` : `${styles.navButton}`}
+    // for default style overriding use more specificity on your variant class
+    >
+      {children}
+    </button>
   )
 }
