0e795c2e785bea640e5ebc7e2585b6e0bbf49ecb
Mindia Arabuli <mindiaarabuli4@gmail.com>
2026-04-12 17:21:03 +0400

    language

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        en: resolve(__dirname, 'en.html'),
+        it: resolve(__dirname, 'it.html'),
+        ka: resolve(__dirname, 'ka.html'),
+      },
+    },
+  },
 })
